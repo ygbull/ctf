11c85a1cdd38769db70100007ac2a276bcd61355
Haijie Qin <jackqin2000@gmail.com>
2024-04-13 16:09:24 -0400

    add hint

--- src/index.php
+++ src/index.php
@@ -2,25 +2,29 @@
 // Check if the referer is from umass.edu
 if (isset($_SERVER['HTTP_REFERER']) && parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) == 'umass.edu') {
     // Allowed, show the HTML content
-?>
-<html>
-<body>
-<form action="upload.php" method="POST" enctype="multipart/form-data">
-    Select PDF file to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload PDF" name="submit">
-</form>
+    ?>
+    <html>
+    <body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        Select PDF file to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload PDF" name="submit">
+    </form>
 
-<form action="verify.php" method="POST">
-    Enter the flag:
-    <label>
-        <input type="text" name="flagInput" required>
-    </label>
-    <input type="submit" value="Verify Flag">
-</form>
-</body>
-</html>
-<?php
+    <form action="verify.php" method="POST">
+        Enter the flag:
+        <label>
+            <input type="text" name="flagInput" required>
+        </label>
+        <input type="submit" value="Verify Flag">
+    </form>
+
+    <!-- hint -->
+    <p style="color: #666; font-size: small;">Hint: The flag is securely stored where system configurations reside.</p>
+
+    </body>
+    </html>
+    <?php
 } else {
     // Not allowed, display an error or redirect
     echo 'Access denied. You must be referred from UMass.edu to access this page.';
